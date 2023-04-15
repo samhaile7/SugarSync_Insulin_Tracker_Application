4d664f29e1f148427303d38e670e22ff7bb109e5
Mohamed Abukar <moworkmain@gmail.com>
2023-04-15 18:14:27 -0400

    pushing

--- vue/src/components/Dashboard.vue
+++ vue/src/components/Dashboard.vue
@@ -1,13 +1,63 @@
 <template>
   <div>
+    <p> Daily Blood Sugar AVG : {{bloodSugarAvgList[0]}} </p>
+       <p> 3 Day : {{bloodSugarAvgList[1]}} </p>
+       <p> 7 day  : {{bloodSugarAvgList[2]}} </p>
+       <p> 2 week  : {{bloodSugarAvgList[3]}} </p>
+
+       <p> Month : {{bloodSugarAvgList[4]}} </p>
+
+       <p> Daily Dosage AVG : {{insulinDosageAvgList[0]}} </p>
+       <p> 3 Day : {{insulinDosageAvgList[1]}} </p>
+       <p> 7 day  : {{insulinDosageAvgList[2]}} </p>
+       <p> 2 week  : {{insulinDosageAvgList[3]}} </p>
+
+       <p> Month : {{insulinDosageAvgList[4]}} </p>
 
 
-      
   </div>
 </template>
 
 <script>
+
+import MealService from "../services/MealService.js";
+
 export default {
+    data() {
+        return {
+            bloodSugarAvgList: [],
+            insulinDosageAvgList: []
+        }
+    },
+
+    methods: {
+        getInsulinDosageAverages() {
+          MealService.getAllInsulinDosageAvg().then((response) => {
+            if (response.status === 200) { 
+
+                   this.insulinDosageAvgList = response.data;
+            }
+          }).catch((err) => console.log(err));
+        },
+
+        getBloodSugarAverages() {
+          MealService.getAllBloodSugarAvg().then((response) => {
+            if (response.status === 200) { 
+
+                   this.bloodSugarAvgList = response.data;
+            }
+        }).catch((err) => console.log(err));
+
+
+
+
+
+    }
+},
+    created() {
+      this.getInsulinDosageAverages();
+      this.getBloodSugarAverages();
+    }
 
 }
 </script>
--- vue/src/services/MealService.js
+++ vue/src/services/MealService.js
@@ -4,7 +4,17 @@ export default{
 
     getAllMeals() {
         return axios.get(`/meal/allmeals`)
+    },
+
+    getAllBloodSugarAvg() {
+        return axios.get(`/bloodsugarhistory`)
+    },
+
+    getAllInsulinDosageAvg() {
+        return axios.get(`/insulindosagehistory`)
     }
+
+
 
 
 
