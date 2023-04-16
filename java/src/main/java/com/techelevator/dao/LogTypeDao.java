9e69a1e6e7eea59af42391e3dc5108082e49a789
Samuel Haile <samhaile129@gmail.com>
2023-04-16 17:01:51 -0400

    Created Logger and Logtype dao interface, created logger dao add method

---
+++ java/src/main/java/com/techelevator/dao/LogTypeDao.java
@@ -0,0 +1,13 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.LogType;
+
+
+import java.util.List;
+
+public interface LogTypeDao {
+
+
+    List<LogType> getAllLogTypes();
+
+}
---
+++ java/src/main/java/com/techelevator/dao/LoggerDao.java
@@ -0,0 +1,21 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Logger;
+import com.techelevator.model.UserInput;
+
+import java.util.List;
+
+public interface LoggerDao {
+
+     Logger addLog(Logger incomingLog );
+
+    boolean updateLog(Logger updateLog, int id);
+
+    Logger getLogByLogId(int logId);
+
+    List<Logger> getAllLogsByUserId(int userId);
+
+    int findIdByUsername (String username);
+
+
+}
