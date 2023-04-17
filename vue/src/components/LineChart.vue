e955c3c495640fdddbb87670ceb92c46e5229ece
Samuel Haile <samhaile129@gmail.com>
2023-04-17 18:56:02 -0400

    Bar chart displaying succesfully

--- vue/src/components/LineChart.vue
+++ vue/src/components/LineChart.vue
@@ -1,4 +1,48 @@
 <template>
-  <canvas id="myChart"></canvas>
+  <div>
 
+    <canvas id = "myChart" width = "400" height = "400"></canvas>
+    </div>
 </template>
+
+<script>
+
+import Chart from 'chart.js/auto';
+
+export default {
+
+  name: 'HelloWorld', 
+  props: {}, 
+  mounted() {
+    console.log('Component Mounted')
+
+    const ctx = document.getElementById('myChart');
+
+    const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+  myChart;
+  }
+
+}
+
+</script>
+
+<style>
+
+</style>
